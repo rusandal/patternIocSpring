79a18be73a33f75933bceef4a3efc1474dd8f3ad
sandal <ru-sandal@yandex.ru>
2022-02-26 22:10:19 +0300

    Modify: DI constructor in xml initBinMethod and destroyBinMethod

--- src/main/java/ru/minikhanov/ChildrensBook.java
+++ src/main/java/ru/minikhanov/ChildrensBook.java
@@ -1,6 +1,14 @@
 package ru.minikhanov;
 
 public class ChildrensBook implements Book {
+    public void doMyInit(){
+        System.out.println("Doing init bean");
+    }
+
+    public void doDestroyBean(){
+        System.out.println("Doing destroy bean");
+    }
+
     @Override
     public String readBook() {
         return "Abc";
