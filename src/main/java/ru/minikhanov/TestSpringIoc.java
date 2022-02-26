7a057203092f158d0ab9c216de57bd70a946cff4
sandal <ru-sandal@yandex.ru>
2022-02-26 20:00:34 +0300

    Modify: DI constructor in xml

--- src/main/java/ru/minikhanov/TestSpringIoc.java
+++ src/main/java/ru/minikhanov/TestSpringIoc.java
@@ -6,10 +6,10 @@ public class TestSpringIoc {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Book book = context.getBean("bookBean", Book.class);
+        //Book book = context.getBean("bookBean", Book.class);
 
-        Library library = new Library(book);
-
+        //Library library = new Library(book);
+        Library library = context.getBean("libraryBean", Library.class);
         library.readBookInLibrary();
 
         context.close();
