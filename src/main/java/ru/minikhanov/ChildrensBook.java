8a852323f0b39b3ee7bdc39399774494d3a6528b
sandal <ru-sandal@yandex.ru>
2022-03-21 11:18:32 +0300

    Modify: added DI (+init, destroy) and readme

--- src/main/java/ru/minikhanov/ChildrensBook.java
+++ src/main/java/ru/minikhanov/ChildrensBook.java
@@ -1,6 +1,14 @@
 package ru.minikhanov;
 
 public class ChildrensBook implements Book {
+    public void doInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    public void doDestroy(){
+        System.out.println("Doing my destroy");
+    }
+
     @Override
     public String readBook() {
         return "Abc";
--- src/main/java/ru/minikhanov/TestSpringIoc.java
+++ src/main/java/ru/minikhanov/TestSpringIoc.java
@@ -6,9 +6,13 @@ public class TestSpringIoc {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Book book = context.getBean("bookBean", Book.class);
+        //Убрал, так как мы создали бин 2-ой бин Library в xml с зависимостью от бина Book
+        //IoC (1 бин без зависимости) Получаем БИН из контекста spring
+        //Book book = context.getBean("bookBean", Book.class);
+        //Library library = new Library(book);
 
-        Library library = new Library(book);
+        //IoC|DI (2 бина с одно зависимостью)
+        Library library = context.getBean("libraryBean", Library.class);
 
         library.readBookInLibrary();
 
